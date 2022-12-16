b0107b73808b64050a2923505b2bdace3ce5c9b1
Alex Taylor <to.alextaylor@outlook.com>
2022-12-16 03:29:21 +0000

    Update coupon_sim.py

--- coupon_sim.py
+++ coupon_sim.py
@@ -1,39 +1,57 @@
 import streamlit as st
-import random
+import numpy as np
 import matplotlib.pyplot as plt
 
-st.title('Coupon Collectors Problem Simulator')
+# Set up the Streamlit app
+st.title("Coupon Collectors Problem Simulator")
 
-# Get input variables
-num_coupons_per_pack = st.sidebar.number_input('Number of coupons per pack:', min_value=1)
-total_num_coupons = st.sidebar.number_input('Total number of coupons:', min_value=1)
+# Get input from the user
+n_coupons = st.sidebar.number_input("Number of coupons", min_value=1, max_value=1000, value=670)
+n_packs = st.sidebar.number_input("Number of coupons per pack", min_value=1, max_value=n_coupons, value=5)
 
-# Initialize variables for simulation
-coupons = set()
-num_packs = 0
+# Function to simulate coupon collectors problem
+def simulate_coupon_collectors(n_coupons, n_pack):
+    coupons = set()
+    packs = 0
+    while len(coupons) < n_coupons:
+        packs += 1
+        coupons.update(np.random.choice(n_coupons, size=n_pack, replace=False))
+    return packs
 
-# Function to simulate getting a pack of coupons
-def get_pack():
-    global num_packs
-    num_packs += 1
-    for i in range(num_coupons_per_pack):
-        coupons.add(random.randint(1, total_num_coupons))
+button_row = st.empty()
 
-# Start button
-if st.button('Start'):
-    while len(coupons) < total_num_coupons:
-        get_pack()
+state = button_row.radio(
+    "State",
+    ('Pause', 'Run', 'Reset'),
+    0
+    horizontal=True
+)
 
-# Pause button
-if st.button('Pause'):
-    pass
-
-# Reset button
-if st.button('Reset'):
-    coupons.clear()
-    num_packs = 0
-
-# Show histogram
+# Create a histogram to display the results of the simulations
+pl = st.empty()
+results = []
 fig, ax = plt.subplots()
-ax.hist(num_packs)
-st.pyplot(fig)
+ax.hist(np.asarray(results))
+pl.pyplot(fig)
+
+while True:
+    if state == 'Running':
+        # Run the simulation and update the histogram
+        result = simulate_coupon_collectors(n_coupons, n_packs)
+        results.append(result)
+        fig, ax = plt.subplots()
+        ax.hist(np.asarray(results))
+        pl.pyplot(fig)
+    elif state == 'Reset':
+        # Reset the histogram data and display an empty histogram
+        results = []
+        fig, ax = plt.subplots()
+        ax.hist(np.asarray(results))
+        pl.pyplot(fig)
+        # Change state back to Pause
+        state = button_row.radio(
+            "State",
+            ('Pause', 'Run', 'Reset'),
+            0
+            horizontal=True
+        )
